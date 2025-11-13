2e5f81f8404578a88c6655539a94466f52acd774
Alexey Karatchentsev <karatchentsev@yandex.ru>
2025-11-13 20:14:14 +0300

    create ai-agent project

---
+++ README.md
@@ -0,0 +1,3 @@
+# agno-ai-bot
+
+a very simple AI bot based on Agno framework
---
+++ agent_01.py
@@ -0,0 +1,13 @@
+import os, sys
+from agno.agent import Agent
+from agno.models.openrouter import OpenRouter
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = (os.getenv('OPENAI_API_KEY') or
+           sys.exit('Error: .env does not contain OPENAI_API_KEY'))
+id_model = os.getenv('ID_MODEL') or sys_exit('Error: .env does not contain ID_MODEL')
+
+agent = Agent(model=OpenRouter(id=id_model))
+
+print(agent)

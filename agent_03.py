0eacca9ac4e03b5d9dfe78f51586840480112a38
Alexey Karachentsev <alexey.karachentsev@vniira.spb.su>
2025-12-02 08:13:59 +0300

    initial agent_03

--- agent_03.py
+++ agent_03.py
@@ -22,40 +22,30 @@ id_model = os.getenv('ID_MODEL') or sys.exit('file .env does not contain ID_MODE
 embedder = SentenceTransformerEmbedder(id='all-MiniLM-L6-v2')
 # embedder = OpenAIEmbedder(id="text-embedding-3-small")
 
-text = "Все тела падают на землю с одинаковым ускорением, равным g."
-vector = embedder.get_embedding(text)
-print(f'Vector dimension: {len(vector)}')
-print(f'First numbers: {vector[:10]}')
-
-text = "Тела с разной массой падают на землю с одинаковым ускорением, равным g."
-vector = embedder.get_embedding(text)
-print(f'Vector dimension: {len(vector)}')
-print(f'First numbers: {vector[:10]}')
-
 # локальная векторная база LanceDB с гибридным поиском
-# vector_db = LanceDb(
-#     table_name='text_documents',  # название таблицы для эмбеддингов
-#     uri='lancedb_storage',  # локальная папка для LanceDb
-#     search_type=SearchType.hybrid,  # гибридный поиск: по смыслу и ключевым словам
-#     embedder=embedder  # подключение эмбеддера
-# )
-#
-# #  создаем knowledge, объект базы знаний
-# knowledge = Knowledge(vector_db=vector_db)
-# knowledge.add_content(path='history.txt')
-#
-# # агент со знаниями
-# agent = Agent(
-#     model=OpenRouter(id=id_model, api_key=api_key),  # подключение модели
-#     # model=Ollama(id="llama3.2"),
-#     knowledge=knowledge,  # подключаем базу знаний
-#     search_knowledge=True, # разрешаем ее использование
-#     debug_mode=True  # выключаем режим отладки (по умолчанию)
-# )
-#
-# # question = 'В каком городе проходила первая конференция Agno? сколько стран в ней участвовало?'
-# question = input('User: ').strip()
-#
-#
-# response = agent.run(question)
-# print(f'Agent: {response.content}')
+vector_db = LanceDb(
+    table_name='text_documents',  # название таблицы для эмбеддингов
+    uri='lancedb_storage',  # локальная папка для LanceDb
+    search_type=SearchType.hybrid,  # гибридный поиск: по смыслу и ключевым словам
+    embedder=embedder  # подключение эмбеддера
+)
+
+#  создаем knowledge, объект базы знаний
+knowledge = Knowledge(vector_db=vector_db)
+knowledge.add_content(path='history.txt')
+
+# агент со знаниями
+agent = Agent(
+    model=OpenRouter(id=id_model, api_key=api_key),  # подключение модели
+    # model=Ollama(id="llama3.2"),
+    knowledge=knowledge,  # подключаем базу знаний
+    search_knowledge=True, # разрешаем ее использование
+    debug_mode=True  # выключаем режим отладки (по умолчанию)
+)
+
+# question = 'В каком городе проходила первая конференция Agno? сколько стран в ней участвовало?'
+question = input('User: ').strip()
+
+
+response = agent.run(question)
+print(f'Agent: {response.content}')
